a876ead410f30907b9f9a7c5c65e58f41ee0698d
Jack Maguire <jackmaguire1444@gmail.com>
2018-04-16 14:57:10 -0400

    first attempt at batching input

--- S_S_hbond/S_S_hbond_training.boolean.batch.py
+++ S_S_hbond/S_S_hbond_training.boolean.batch.py
@@ -11,6 +11,7 @@ import numpy
 import sys
 
 import argparse
+import random
 
 ########
 # INIT #
@@ -49,8 +50,8 @@ DIST   = int( 12 )
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument( "--train", help="Input data file for training", required=True )
-parser.add_argument( "--test", help="Input data file for testing", required=True )
+#parser.add_argument( "--train", help="Input data file for training", required=True )
+#parser.add_argument( "--test", help="Input data file for testing", required=True )
 
 parser.add_argument( "--num_neurons_in_first_hidden_layer", help="Number of neruons for first hidden layer.", default="100", type=int, required=False )
 parser.add_argument( "--num_neurons_in_intermediate_hidden_layer", help="Number of neruons for intermediate hidden layer.", default="100", type=int, required=False )
@@ -63,15 +64,15 @@ parser.add_argument( "--test_predictions", help="filename for test predictions",
 
 args = parser.parse_args()
 
-if( args.train ):
-    train_datafilename = args.train
-else:
-    print( "The --train argument is required" )
-    parser.parse_args( ['-h'] )
-    exit( 0 )
+#if( args.train ):
+#    train_datafilename = args.train
+#else:
+#    print( "The --train argument is required" )
+#    parser.parse_args( ['-h'] )
+#    exit( 0 )
 
-test_datafilename = args.test
-print( "test_datafilename: " + test_datafilename )
+#test_datafilename = args.test
+#print( "test_datafilename: " + test_datafilename )
 
 num_neurons_in_first_hidden_layer = args.num_neurons_in_first_hidden_layer
 print( "num_neurons_in_first_hidden_layer: " + str( num_neurons_in_first_hidden_layer ) )
@@ -114,6 +115,20 @@ def keep_hbond_score( score ):
         return true
     return false
 
+def generate_data_from_file( filename ):
+    dataset = numpy.genfromtxt( filename, delimiter=",", skip_header=0 )
+
+    input = training_dataset[:,[ TX, TY, TZ, RX, RY, RZ, ANGLE1, ANGLE2, DIST ] ]
+    output_hbond = training_dataset[:,[ BEST_POSSIBLE_HBOND_SCORE  ] ]
+
+    for x in output_hbond:
+        for i in range( 0, len(x) ):
+            if x[i] != 0:
+                x[i] = 1
+
+    return input, output_hbond
+
+
 ###########
 # CLASSES #
 ###########
@@ -135,46 +150,14 @@ def mean_pred( y_true, y_pred ):
 # START #
 #########
 
-# 1) Generate Data
-
-training_dataset = numpy.genfromtxt( train_datafilename, delimiter=",", skip_header=0 )
-#training_dataset = [x for x in training_dataset if keep_hbond_score( x ) ]
-
-training_input = training_dataset[:,[ TX, TY, TZ, RX, RY, RZ, ANGLE1, ANGLE2, DIST ] ]
-training_output_hbond = training_dataset[:,[ BEST_POSSIBLE_HBOND_SCORE  ] ]
-
-for x in training_output_hbond:
-    for i in range( 0, len(x) ):
-        if x[i] != 0:
-            x[i] = 1
-
-my_assert_equals( "len(input)", len(training_input), len(training_output_hbond) )
-
-del training_dataset
-
-test_dataset = numpy.genfromtxt( test_datafilename, delimiter=",", skip_header=0 )
-#test_dataset = [x for x in test_dataset if keep_hbond_score( x ) ]
-
-test_input = test_dataset[:,[ TX, TY, TZ, RX, RY, RZ, ANGLE1, ANGLE2, DIST ] ]
-test_output_hbond = test_dataset[:,[ BEST_POSSIBLE_HBOND_SCORE  ] ]
-
-for x in test_output_hbond:
-    for i in range( 0, len(x) ):
-        if x[i] != 0:
-            x[i] = 1
-
-my_assert_equals( "len(input)", len(test_input), len(test_output_hbond) )
-
-del test_dataset
-
-print( "Training on " + str( len (training_input) ) + " elements" )
-print( "Testing on "  + str( len (test_input)     ) + " elements" )
+# 1) Define Filenames
+input_file_path = "/Volumes/My Book/tensorflow_hbonds_and_clashes/S_S/no_middle_hbond_data/LOG_no_middle_data_split/"
+training_input_files = [ "split_aa", "split_ab", "split_ac", "split_ad", "split_ae", "split_af", "split_ag" ]
+testing_input_files = [ "split_ah", "split_ai", "split_aj" ]
 
 # 2) Define Model
 
-num_input_dimensions = len( training_input[0] )
-my_assert_equals( "num_input_dimensions", num_input_dimensions, 9 )
-
+num_input_dimensions = 9
 model = Sequential()
 
 model.add( Dense( num_neurons_in_first_hidden_layer, input_dim=num_input_dimensions, activation='relu') )
@@ -191,19 +174,16 @@ metrics_to_output=[ 'accuracy' ]
 model.compile( loss='binary_crossentropy', optimizer='adam', metrics=metrics_to_output )
 
 # 4) Fit Model
-history = LossHistory()
-model.fit( x=training_input, y=training_output_hbond, epochs=num_epochs, batch_size=my_batch_size, shuffle=False, callbacks=[history], validation_data=(test_input, test_output_hbond), class_weight={0:1, 1:1000} )
+#history = LossHistory()
+#model.fit( x=training_input, y=training_output_hbond, epochs=num_epochs, batch_size=my_batch_size, shuffle=False, callbacks=[history], validation_data=(test_input, test_output_hbond), class_weight={0:1, 1:1000} )
 
-
-# 5) Evaluate Model
-print(history.losses)
-
-if test_input is None:
-    #what should we do here?
-    print( "No data to test" )
-else:
-    scores = model.evaluate( test_input, test_output_hbond )
-    print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
+for x in range( 0, num_epochs ):
+    random.shuffle( training_input_files )
+    for training_input_filename in training_input_files:
+        training_input_temp, training_output_hbond_temp = generate_data_from_file( input_file_path + training_input_filename )
+        model.train_on_batch( x=training_input_temp, y=training_output_hbond_temp, class_weight={0:1, 1:1000} )
+    if ( x % 25 == 0 ):
+        model.save( "epoch_" + str(x) + ".h5" )
 
 # 6) Save Model
 model.save( "model.h5" )
@@ -211,11 +191,13 @@ model.save( "model.h5" )
 # 7) Print Predicitons
 if( len(test_predictions) > 0 ):
     test_predictions_file = open ( test_predictions, "w" )
-    for i in range( 0, len( test_input ) ):
-        temp_array = numpy.zeros( shape=( 9, 1 ) )
-        for j in range( 0, 9 ):
-            temp_array[ j ][ 0 ] = test_input[ i ][ j ]
-        actual = test_output_hbond[ i ][ 0 ]
-        prediction = model.predict( numpy.transpose( temp_array ) )
-        test_predictions_file.write( str( actual ) + "\t" + str( prediction[0][0] ) + "\n" )
+    for test_filename in testing_input_files:
+        test_input, test_output_hbond = generate_data_from_file( input_file_path + test_filename )
+        for i in range( 0, len( test_input ) ):
+            temp_array = numpy.zeros( shape=( 9, 1 ) )
+            for j in range( 0, 9 ):
+                temp_array[ j ][ 0 ] = test_input[ i ][ j ]
+            actual = test_output_hbond[ i ][ 0 ]
+            prediction = model.predict( numpy.transpose( temp_array ) )
+            test_predictions_file.write( str( actual ) + "\t" + str( prediction[0][0] ) + "\n" )
 
