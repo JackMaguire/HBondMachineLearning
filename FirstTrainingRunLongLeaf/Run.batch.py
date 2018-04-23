4c6f3e6d182d11df03fc89fed310f5cb53ef6fe5
Jack <jackmaguire1444@gmail.com>
2018-04-23 11:08:25 -0400

    FirstTrainingRunLongLeaf/Run.batch.py now disables gpu

--- FirstTrainingRunLongLeaf/Run.batch.py
+++ FirstTrainingRunLongLeaf/Run.batch.py
@@ -1,3 +1,7 @@
+import os
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+os.environ["CUDA_VISIBLE_DEVICES"] = ""
+
 #from keras import *
 from keras.models import Sequential
 from keras.layers import Dense
