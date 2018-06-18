f68813d88278a00f5afa19964168d34281efc0bf
Jack Maguire <jack@med.unc.edu>
2018-06-18 09:13:28 -0400

    Eval table

---
+++ HashTableAlternative/EvaluateTable.java
@@ -0,0 +1,15 @@
+import java.io.*;
+
+public class EvaluateTable {
+
+    public static void main( String[] args ){
+
+	//args:
+	//0: table
+	//1-N: data files
+
+	Table t = new Table( args[ 0 ] );
+
+    }
+
+}
